534794c99e0850c0fd0e3c2256c387ded9008de6
Kevin Martínez <n4utilius@gmail.com>
2013-06-18 16:27:08 -0500

    añadir a query getTorneos

--- src/bdGF/SampleDB.java
+++ src/bdGF/SampleDB.java
@@ -69,7 +69,7 @@ public class SampleDB
     
     public JSONArray getTorneos() throws SQLException{
       	
-     String sql = "SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.descripcion as idCategoria, " +
+     String sql = "SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.id as idCategoria, categoria.descripcion as nombreCategoria, " +
      		      "       torneo.fecha_ini as fechaInicio, torneo.usuario as idUsuario, usuario.nick as nombreUsuario, " +
      		      "       torneo.modalidad as idModalidad, Modalidad.descripcion as nombreModalidad, torneo.cancha as idCancha ," +
      		      "       Canchas.descripcion as nombreCancha " +
@@ -87,20 +87,48 @@ public class SampleDB
     
     public JSONArray getUserTorneos(String idUsuario) throws SQLException{
     	
-     String sql = "SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.descripcion as idCategoria, " +
-     		      "       torneo.fecha_ini as fechaInicio, torneo.usuario as idUsuario, usuario.nick as nombreUsuario, " +
-     		      "       torneo.modalidad as idModalidad, Modalidad.descripcion as nombreModalidad, torneo.cancha as idCancha ," +
+     String sql = " SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.id as idCategoria, categoria.descripcion as nombreCategoria, " +
+     		      "        torneo.fecha_ini as fechaInicio, torneo.usuario as idUsuario, usuario.nick as nombreUsuario, " +
+     		      "        torneo.modalidad as idModalidad, Modalidad.descripcion as nombreModalidad, torneo.cancha as idCancha ," +
      		      "       Canchas.descripcion as nombreCancha " +
-     			  " FROM torneo, categoria, usuario, Modalidad,Canchas" +
-     			  " WHERE torneo.rama = categoria.id AND " +
-     			  "      torneo.usuario = usuario.id_usuario AND " +
-     			  "      torneo.modalidad = Modalidad.id  AND"+
-     			  "		 torneo.cancha=Canchas.id AND " + 
-	  			  "		 torneo.usuario = " + idUsuario;
+     			  "  FROM torneo, categoria, usuario, Modalidad,Canchas" +
+     			  "  WHERE torneo.rama = categoria.id AND " +
+     			  "       torneo.usuario = usuario.id_usuario AND " +
+     			  "       torneo.modalidad = Modalidad.id  AND"+
+     			  "		  torneo.cancha=Canchas.id AND " + 
+	  			  "		  torneo.usuario = " + idUsuario;
      
      
     	
      //id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario modalidad cancha activo
      return getqueryGen(sql);
     }
+/*
+    public boolean insertTorneo(String nombreTorneo, String rama, String fechaInicio, String fechaFin, 
+    							  String usuario, String modalidad, String cancha) throws SQLException{
+    	Statement stmt = null;
+    	boolean success= false;
+    	
+    	stmt = conn.createStatement();
+    	//stmt = con.prepareStatement(" INSERT INTO torneo (nombre_torneo, rama, fecha_ini, fecha_fin, usuario, modalidad, cancha, activo) " +
+    	stmt = con.prepareStatement(" INSERT INTO torneo (nombre_torneo, rama, usuario, modalidad, cancha, activo) " +
+    				 				" VALUES (?,?,?,?,?,?,?,?)");
+    	try {
+    		stmt.setString(1, nombreTorneo);
+        	stmt.setInt(2, rama);
+        	//stmt.setString(3, fechaInicio);
+        	//stmt.setString(4, fechaFin);
+        	stmt.setInt(5, Integer.parseInt(usuario));
+        	stmt.setInt(6, Integer.parseInt(modalidad));
+        	stmt.setInt(7, Integer.parseInt(cancha));
+        	stmt.setString(8, "S");
+        	stmt.executeUpdate();
+        	success = true;
+    	} finally {
+    		
+    	}
+	    
+	    return success;
+    }
+    							  */
 }
