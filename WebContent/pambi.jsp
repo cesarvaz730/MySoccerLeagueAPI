dd09aa27db4ee34f618906238cd273915d5c6354
CESAR VAZQUEZ <cesarvaz730@gmail.com>
2013-06-30 18:11:23 -0500

    version final para demo day

--- WebContent/pambi.jsp
+++ WebContent/pambi.jsp
@@ -2,9 +2,7 @@
 	import="
 		net.sf.json.*,
 		bdGF.*,
-		java.util.*
-		"
-	
+		java.util.*"	
 %>
 <%
 String informacion = (request.getParameter("informacion")!=null)?request.getParameter("informacion"):"";
@@ -45,6 +43,16 @@ if (informacion.equals("ejemploMSL")) {
 		
 		infoRegresar =	jsonObjRet.toString();
 	}
+else if (informacion.equals("CatalogoEquipos")) {
+	String idTorneo = (request.getParameter("idTorneo")!=null)?request.getParameter("idTorneo"):"";	
+	JSONObject jsonObjRet= new JSONObject();
+		jsonObjRet.put("success", new Boolean(true));
+		jsonObjRet.put("nombre_peticion",informacion);
+		jsonObjRet.put("registros", db1.getEquiposTorneo(idTorneo).toString());
+		
+		infoRegresar =	jsonObjRet.toString();
+		
+	}
 	else if (informacion.equals("CatalogoTorneos")) {
 		JSONObject jsonObjRet= new JSONObject();
 		jsonObjRet.put("success", new Boolean(true));
@@ -54,7 +62,7 @@ if (informacion.equals("ejemploMSL")) {
 		infoRegresar =	jsonObjRet.toString();
 		
 	}else if (informacion.equals("UsuarioCatalogoTorneos")) {
-		String idUsuario = (request.getParameter("idUsuario")!=null)?request.getParameter("id_Usuario"):"";
+		String idUsuario = (request.getParameter("idUsuario")!=null)?request.getParameter("idUsuario"):"";
 		JSONObject jsonObjRet= new JSONObject();
 		jsonObjRet.put("success", new Boolean(true));
 		jsonObjRet.put("nombre_peticion",informacion);
@@ -85,15 +93,62 @@ if (informacion.equals("ejemploMSL")) {
 		jsonObjRet.put("registros", new Boolean(insertTorneo.insertGenerico(camposTabla,"torneo")));
 		
 		infoRegresar =	jsonObjRet.toString();
-	} else{
+	}else if (informacion.equals("agregaEquipo")) {
+		String idTorneo = (request.getParameter("idTorneo")!=null)?request.getParameter("idTorneo"):"";
+		String nombreEquipo = (request.getParameter("nombreEquipo")!=null)?request.getParameter("nombreEquipo"):"";
+		
+		InsertsDB insertTorneo=new InsertsDB();
+		
+		JSONObject jsonObjRet= new JSONObject();
+		jsonObjRet.put("success", new Boolean(true));
+		jsonObjRet.put("nombre_peticion",informacion);
+		
+		HashMap camposTabla = new HashMap();
+		camposTabla.put("nombre_equipo",nombreEquipo);
+		camposTabla.put("status","S");
+		camposTabla.put("torneo",idTorneo);		
+        
+		jsonObjRet.put("registros", new Boolean(insertTorneo.insertGenerico(camposTabla,"equipo")));
+		
+		infoRegresar =	jsonObjRet.toString();
+	} else if (informacion.equals("registraUsuario")) {
+		String mail = (request.getParameter("mail")!=null)?request.getParameter("mail"):"";
+		String token = (request.getParameter("token")!=null)?request.getParameter("token"):"";
+		JSONObject jsonObjRet= new JSONObject();
+		jsonObjRet.put("success", new Boolean(true));
+		jsonObjRet.put("nombre_peticion",informacion);
+		if(db1.existeUser("'"+token+"'","'"+mail+"'"))
+		{
+			SampleDB db2=new SampleDB();
+			jsonObjRet.put("resultadoID", db2.getIdUser("'"+token+"'","'"+mail+"'"));
+			jsonObjRet.put("resultadoReg", true);
+		}
+		else
+		{
+			InsertsDB insertTorneo=new InsertsDB();
+					
+			HashMap camposTabla = new HashMap();
+			camposTabla.put("nick",mail);
+			camposTabla.put("token",token);
+				if(new Boolean(insertTorneo.insertGenerico(camposTabla,"usuario")))
+				{
+					SampleDB db3=new SampleDB();
+					jsonObjRet.put("resultadoID", db3.getIdUser("'"+token+"'","'"+mail+"'"));
+					jsonObjRet.put("resultadoReg", true);
+				}
+				else
+				{
+					jsonObjRet.put("resultadoReg", false);
+				}
+		}
+		infoRegresar =	jsonObjRet.toString();
+	}else{
 		JSONObject jsonObjRet= new JSONObject();
 		jsonObjRet.put("success", new Boolean(true));
 		jsonObjRet.put("parametro1", "mySoccerleague prueba");
 		jsonObjRet.put("nombre_peticion",informacion);
-		jsonObjRet.put("parametro2", "pruebas viejas");
-		
-		infoRegresar =	jsonObjRet.toString();
-		
+		jsonObjRet.put("parametro2", "pruebas viejas");		
+		infoRegresar =	jsonObjRet.toString();		
 	}
 	
 	
--- src/bdGF/SampleDB.java
+++ src/bdGF/SampleDB.java
@@ -1,4 +1,5 @@
 package bdGF;
+
 import java.sql.*;
 
 import com.adventnet.zoho.client.report.ReportClient;
@@ -6,107 +7,202 @@ import com.adventnet.zoho.client.report.ReportClient;
 import bdGF.utils.ResultSetConverter;
 import net.sf.json.*;
 
-public class SampleDB
-{  
-    
-   
-    private  Connection conn = null;
-   
-    public SampleDB(){
-    	try {
-    		conn=(new Conexion()).getConn();
+public class SampleDB {
+
+	private Connection conn = null;
+
+	public SampleDB() {
+		try {
+			conn = (new Conexion()).getConn();
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
-    }
-    
-   
-    /**
-     * Query generico
-     */
-    public JSONArray getqueryGen(String Query) throws SQLException
-    {
-    	Statement stmt = null;
-        ResultSet rs = null;
-        JSONArray resultado=null;
-       
-     try{   
-         stmt = conn.createStatement();
-         String sql = Query;
-         rs = stmt.executeQuery(sql);
-         resultado=ResultSetConverter.convert(rs);
-     } 
-     finally
-     {
-         if (rs!= null){try { rs.close(); } catch (Exception e){ e.printStackTrace();}}
-         if (rs!= null){try { stmt.close(); } catch (Exception e){ e.printStackTrace();}}
-         if (rs!= null){try { conn.close(); } catch (Exception e){ e.printStackTrace();}}
-     }
-    	return resultado;
-    }
-    /**
-     * query para catalogo canchas
-     */
-    public JSONArray getCanchas() throws SQLException
-    {
-     String sql = "select * from Canchas order by descripcion";
-     return getqueryGen(sql);
-    }
-    /**
-     * query para catalogo categorias
-     */
-    public JSONArray getCategorias() throws SQLException
-    {    
-     String sql = "select * from categoria order by descripcion";
-     return getqueryGen(sql);
-    }
-    /**
-     * query para catalogo modalidad
-     */
-    public JSONArray getModalidades() throws SQLException
-    {     
-     String sql = "select * from Modalidad order by descripcion";
-     return getqueryGen(sql);
-    }
-    
-    public JSONArray getTorneos() throws SQLException{
-      	
-     String sql = "SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.id as idCategoria, categoria.descripcion as nombreCategoria, " +
-     		      "       torneo.fecha_ini as fechaInicio, torneo.usuario as idUsuario, usuario.nick as nombreUsuario, " +
-     		      "       torneo.modalidad as idModalidad, Modalidad.descripcion as nombreModalidad, torneo.cancha as idCancha ," +
-     		      "       Canchas.descripcion as nombreCancha " +
-     			  " FROM torneo, categoria, usuario, Modalidad,Canchas" +
-     			  " WHERE torneo.rama = categoria.id AND " +
-     			  "      torneo.usuario = usuario.id_usuario AND " +
-     			  "      torneo.modalidad = Modalidad.id  AND"+
-     			  "		 torneo.cancha=Canchas.id";
-     
-     
-    	
-     //id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario modalidad cancha activo
-     return getqueryGen(sql);
-    }
-    
-    public JSONArray getUserTorneos(String idUsuario) throws SQLException{
-    	
-     String sql = " SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.id as idCategoria, categoria.descripcion as nombreCategoria, " +
-     		      "        torneo.fecha_ini as fechaInicio, torneo.usuario as idUsuario, usuario.nick as nombreUsuario, " +
-     		      "        torneo.modalidad as idModalidad, Modalidad.descripcion as nombreModalidad, torneo.cancha as idCancha ," +
-     		      "       Canchas.descripcion as nombreCancha " +
-     			  "  FROM torneo, categoria, usuario, Modalidad,Canchas" +
-     			  "  WHERE torneo.rama = categoria.id AND " +
-     			  "       torneo.usuario = usuario.id_usuario AND " +
-     			  "       torneo.modalidad = Modalidad.id  AND"+
-     			  "		  torneo.cancha=Canchas.id AND " + 
-	  			  "		  torneo.usuario = " + idUsuario;
-     
-     
-    	
-     //id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario modalidad cancha activo
-     return getqueryGen(sql);
-    }
-    
-   
+	}
+
+	/**
+	 * Query generico
+	 */
+	public JSONArray getqueryGen(String Query) throws SQLException {
+		Statement stmt = null;
+		ResultSet rs = null;
+		JSONArray resultado = null;
+
+		try {
+			stmt = conn.createStatement();
+			String sql = Query;
+			rs = stmt.executeQuery(sql);
+			resultado = ResultSetConverter.convert(rs);
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					stmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return resultado;
+	}
+
+	/**
+	 * Query generico result
+	 */
+	public ResultSet getqueryGenRs(String Query, Statement stmt)
+			throws SQLException {
+
+		ResultSet rs = null;
+
+		try {
+			stmt = conn.createStatement();
+			String sql = Query;
+			rs = stmt.executeQuery(sql);
+
+		} finally {
+			;
+		}
+		return rs;
+	}
+
+	/**
+	 * query para catalogo canchas
+	 */
+	public JSONArray getCanchas() throws SQLException {
+		String sql = "select * from Canchas order by descripcion";
+		return getqueryGen(sql);
+	}
+
+	/**
+	 * query para catalogo categorias
+	 */
+	public JSONArray getCategorias() throws SQLException {
+		String sql = "select * from categoria order by descripcion";
+		return getqueryGen(sql);
+	}
+
+	/**
+	 * query para catalogo modalidad
+	 */
+	public JSONArray getModalidades() throws SQLException {
+		String sql = "select * from Modalidad order by descripcion";
+		return getqueryGen(sql);
+	}
+
+	public JSONArray getTorneos() throws SQLException {
+
+		String sql = "SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.id as idCategoria, categoria.descripcion as nombreCategoria, "
+				+ "       torneo.fecha_ini as fechaInicio, torneo.usuario as idUsuario, usuario.nick as nombreUsuario, "
+				+ "       torneo.modalidad as idModalidad, Modalidad.descripcion as nombreModalidad, torneo.cancha as idCancha ,"
+				+ "       Canchas.descripcion as nombreCancha "
+				+ " FROM torneo, categoria, usuario, Modalidad,Canchas"
+				+ " WHERE torneo.rama = categoria.id AND "
+				+ "      torneo.usuario = usuario.id_usuario AND "
+				+ "      torneo.modalidad = Modalidad.id  AND"
+				+ "		 torneo.cancha=Canchas.id";
+
+		// id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario
+		// modalidad cancha activo
+		return getqueryGen(sql);
+	}
+
+	public JSONArray getUserTorneos(String idUsuario) throws SQLException {
+
+		String sql = " SELECT torneo.id_torneo as idTorneo, torneo.nombre_torneo as nombreTorneo, categoria.id as idCategoria, categoria.descripcion as nombreCategoria, "
+				+ "        torneo.fecha_ini as fechaInicio, torneo.usuario as idUsuario, usuario.nick as nombreUsuario, "
+				+ "        torneo.modalidad as idModalidad, Modalidad.descripcion as nombreModalidad, torneo.cancha as idCancha ,"
+				+ "       Canchas.descripcion as nombreCancha "
+				+ "  FROM torneo, categoria, usuario, Modalidad,Canchas"
+				+ "  WHERE torneo.rama = categoria.id AND "
+				+ "       torneo.usuario = usuario.id_usuario AND "
+				+ "       torneo.modalidad = Modalidad.id  AND"
+				+ "		  torneo.cancha=Canchas.id AND "
+				+ "		  torneo.usuario = "
+				+ idUsuario;
+
+		// id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario
+		// modalidad cancha activo
+		return getqueryGen(sql);
+	}
+
+	public JSONArray getEquiposTorneo(String idTorneo) throws SQLException {
+
+		String sql = "SELECT id_equipo as idEquipo,nombre_equipo as nombreEquipo "
+				+ " FROM equipo "
+				+ " WHERE status='S' AND torneo = "
+				+ idTorneo;
+
+		// id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario
+		// modalidad cancha activo
+		return getqueryGen(sql);
+	}
+
+	public boolean existeUser(String token, String mail) {
+
+		String sql = "SELECT count(*) as conteo " + " FROM usuario "
+				+ " WHERE nick=" + mail + " AND token = " + token;
+
+		boolean bRes = false;
+		ResultSet rs = null;
+		Statement stmt = null;
+		// id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario
+		// modalidad cancha activo
+		try {
+			rs = getqueryGenRs(sql, stmt);
+			rs.next();
+			String s = rs.getString("conteo");
+			if (s.equals("0"))
+				bRes = false;
+			else {
+				bRes = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {try {rs.close();} catch (Exception e) {e.printStackTrace();}}
+			if (rs != null && stmt != null) {try {stmt.close();} catch (Exception e) {e.printStackTrace();}}
+			if (rs != null) {try {conn.close();} catch (Exception e) {e.printStackTrace();}}
+		}
+		return bRes;
+	}
+	public String getIdUser(String token, String mail) {
+
+		String sql = "SELECT id_usuario as is_user " + " FROM usuario "
+				+ " WHERE nick=" + mail + " AND token = " + token;
+
+		String bRes = "";
+		ResultSet rs = null;
+		Statement stmt = null;
+		// id_torneo nombre_torneo rama logo fecha_ini fecha_fin usuario
+		// modalidad cancha activo
+		try {
+			rs = getqueryGenRs(sql, stmt);
+			rs.next();
+			bRes = rs.getString("is_user");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {try {rs.close();} catch (Exception e) {e.printStackTrace();}}
+			if (rs != null && stmt != null) {try {stmt.close();} catch (Exception e) {e.printStackTrace();}}
+			if (rs != null) {try {conn.close();} catch (Exception e) {e.printStackTrace();}}
+		}
+		return bRes;
+	}
 
 }
